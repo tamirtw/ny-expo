8c954aa3168ce85dd2c7f06d10b8b39e69e8748b
tamirtw <tamirtw@gmail.com>
2014-03-25 22:18:57 +0200

    try to fix db insert error

--- php/index.php
+++ php/index.php
@@ -20,9 +20,11 @@ if($_POST['email']){
 			throw new Exception('Invalid Email!');
 		}
 		$db = new PDO('sqlite:' . getenv('OPENSHIFT_DATA_DIR') . 'leads.sqlite;dbname=leads');
+		print_r($db);
 		$sql = "INSERT INTO leads (email,project) VALUES (?,?)";
 		$email = $_POST['email'];
 		$q = $db->prepare($sql);
+		echo "Email:" . $email;
 		$q->execute(array($email,$project));
 
 
